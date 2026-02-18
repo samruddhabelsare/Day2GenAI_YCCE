909d72090fdd789bd0c646ef611106d6d97e809f
Samruddha Belsare <ujjwalabelsare123@gmail.com>
2026-02-18 12:18:03 +0530

    step 3 --> load ollama model
    loaded ollama model and connected it with pipelines

--- ollamachatbotdemo.py
+++ ollamachatbotdemo.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 #import python builtin os 
 
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
@@ -18,10 +18,10 @@ prompt = ChatPromptTemplate.from_messages(
         # system message defines ai behaviour 
         ("system","you are a helpful assistant , please respond clearly to questions asked "),
         #user message contains placeholder {question}
-        ("user ","Question : {question}") 
+        ("user","Question : {question}") 
     ]
 )
-# step 2 - streamlit app UI
+# step 2 --> streamlit app UI
 
 # streamlit title 
 st.title("Langchain Demo Model(Ollama)")
@@ -32,7 +32,7 @@ input_txt = st.text_input("What is your question ")
 # step 3 --> load ollama model 
 
 # load local gemma model
-LLM = ollama(model="gemma2:2b")
+LLM = Ollama(model="gemma2:2b")
 
 # Condition - convert output model to string 
 output_parser = StrOutputParser()
@@ -40,4 +40,7 @@ output_parser = StrOutputParser()
 # create langchain pipeline (prompt --> model --> output_parser )
 chain = prompt | LLM | output_parser 
 
- 
+# step 4 --> run the model when user inputs the question
+if input_txt:
+    response = chain.invoke({"question":input_txt})
+    st.write(response)
